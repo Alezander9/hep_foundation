3c1e5fe9a7eb158853f3911d1af3d8b3b0344402
Alezander9 <alexander.j.yue@gmail.com>
2025-07-28 15:55:36 -0700

    address cubic comment

--- vulture_whitelist.py
+++ vulture_whitelist.py
@@ -2,20 +2,17 @@
 
 # Plot utils are for standardization of plots (including future plots) so is okay if not used
 from src.hep_foundation.plots import plot_utils
-
 plot_utils.get_model_line_style
 plot_utils.get_available_line_styles
 
 # Keras Layer methods that are called implicitly by the framework
 from src.hep_foundation.models.variational_autoencoder import VAELayer
-
 VAELayer.call
 VAELayer.on_epoch_begin
 VAELayer.on_epoch_end
 
 # Model trainer methods are called implicitly by the framework
 from src.hep_foundation.training import model_trainer
-
 model_trainer.TrainingProgressCallback.on_train_begin
 model_trainer.TrainingProgressCallback.on_epoch_begin
 model_trainer.TrainingProgressCallback.on_epoch_end
@@ -25,13 +22,12 @@ model_trainer.TrainingProgressCallback.on_epoch_end
 # fine-tuned vs fixed encoder models in foundation model evaluation
 # We need to simulate accessing the trainable attribute on model objects
 import tensorflow as tf
-
-dummy_model = tf.keras.Model()
-dummy_model.trainable  # Simulates usage of .trainable attribute
+# Use a layer instead of Model to avoid constructor issues
+dummy_layer = tf.keras.layers.Dense(1)
+dummy_layer.trainable  # Simulates usage of .trainable attribute
 
 # YAML serialization functionality
 from src.hep_foundation.utils.utils import ConfigSerializer
-
 # This prevents YAML aliases which can cause config serialization issues
 # We need to simulate accessing the ignore_aliases attribute on the class
 ConfigSerializer.ignore_aliases
